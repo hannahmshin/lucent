23e28f98e0a830f255b1a291191e0a1661974e21
Hannah Kim <hkim@rmn.com>
2020-03-31 18:40:02 -0500

    chore: refactor file structure for client

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+import logo from './logo.svg';
+import './App.css';
+
+const client = new W3CWebSocket('ws://127.0.0.1:8000');
+
+class App extends Component {
+  componentWillMount() {
+    client.onopen = () => {
+      console.log('WebSocket Client Connected');
+    };
+    client.onmessage = (message) => {
+      console.log(message);
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+        </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+        </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
