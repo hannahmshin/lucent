afe35b1b4b7ddb502fd31dad8c362c9281a114b7
Ryan Regalado <ryanreg48@gmail.com>
2020-04-01 17:56:15 -0700

    feat(Preview): add buttons for increase and decrease speed:

--- client/src/components/Preview.js
+++ client/src/components/Preview.js
@@ -1,14 +1,35 @@
 import React from 'react';
 
 function Preview() {
+  let lightSpeed = 2;
+
+  const increaseSpeed = () => {
+    const lightElem = document.getElementById("elem");
+    if (lightSpeed > 1) {
+      lightSpeed -= 1;
+    }
+    lightElem.style = 'animation: ani ' + lightSpeed + 's infinite';
+  };
+
+
+  const decreaseSpeed = () => {
+    const lightElem = document.getElementById("elem");
+    lightSpeed += 1;
+    lightElem.style = 'animation: ani ' + lightSpeed + 's infinite';
+  }
+
   return (
     <div>
       <h2>Preview</h2>
 
+      <nav>
+        <button id="increase-speed" onClick={increaseSpeed}> + Increase speed</button>
+        <button id="decrease-speed" onClick={decreaseSpeed}> - Decrease speed</button>
+      </nav>
 
-      <div class="container">
-        <div class="path">
-          <span id="elem" class="shape trail"></span>
+      <div className="container">
+        <div className="path">
+          <span id="elem" className="shape trail"></span>
         </div>
       </div>
 
