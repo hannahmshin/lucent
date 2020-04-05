0777e87360ee01bd45753e98bf43e59eee0f7e18
Ryan Regalado <ryanreg48@gmail.com>
2020-04-04 20:01:51 -0700

    feat(AppHeader): wip profile area

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import logoUrl from './static/logo-header.png'
+import logoUrl from './static/logo-header.png';
+import profilePicUrl from './static/james-kelly.jpeg';
+import dotMenuUrl from './static/dots-menu.png';
 import './AppHeader.css';
 import './AppHeaderMenu.css';
 
@@ -23,6 +25,13 @@ const AppHeader = () => {
           </div>
         </div>
         <img className="logo-header" src={logoUrl} alt="LUCENT logo" />
+        <div id="profile">
+          <span id="name">
+            <span>Welcome back </span><span>James K</span>
+          </span>
+          <img src={profilePicUrl} alt="James K" />
+          <img src={dotMenuUrl} alt="Profile menu" />
+        </div>
         <section className="client-menu" id="client-menu"></section>
       </header>
     </>
