71f59ecf0e43c9742eb0e415ce364e948728875f
Ryan Regalado <ryanreg48@gmail.com>
2020-04-04 19:34:37 -0700

    feat(AppHeader): enable menu functionality

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -4,11 +4,20 @@ import './AppHeader.css';
 import './AppHeaderMenu.css';
 
 const AppHeader = () => {
+  let isActive = false;
+
+  const toggleMenu = (event) => {
+    const el = document.getElementById("hamburger-menu");
+    const menuElement = document.getElementById("client-menu");
+    el.classList.toggle("is-active");
+    menuElement.classList.toggle("change");
+  };
+
   return (
     <>
       <section className="top-header"></section>
       <header className="App-header">
-        <div id="hamburger-menu" className="hamburger hamburger--arrow js-hamburger">
+        <div id="hamburger-menu" className="hamburger hamburger--arrow js-hamburger" onClick={toggleMenu}>
           <div className="hamburger-box">
             <div className="hamburger-inner"></div>
           </div>
