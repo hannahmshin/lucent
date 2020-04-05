ce743320a526057fce2c0964affb4467c588bf54
Ryan Regalado <ryanreg48@gmail.com>
2020-04-04 19:25:44 -0700

    feat(AppHeader): base styling with hamburger menu

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import logoUrl from './static/logo-header.png'
+import './AppHeader.css';
+import './AppHeaderMenu.css';
 
 const AppHeader = () => {
   return (
     <>
       <section className="top-header"></section>
       <header className="App-header">
+        <div id="hamburger-menu" className="hamburger hamburger--arrow js-hamburger">
+          <div className="hamburger-box">
+            <div className="hamburger-inner"></div>
+          </div>
+        </div>
+        <img className="logo-header" src={logoUrl} alt="LUCENT logo" />
+        <section className="client-menu" id="client-menu"></section>
       </header>
     </>
   )
